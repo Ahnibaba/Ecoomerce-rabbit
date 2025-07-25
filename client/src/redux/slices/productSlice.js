057ca2e03dad4937a5eb8b2a062af958fe53da5e
Ahnibaba <aniebietedet2025@gmail.com>
2025-07-25 17:05:28 +0100

    productSlice

--- client/src/redux/slices/productSlice.js
+++ client/src/redux/slices/productSlice.js
@@ -4,76 +4,132 @@ import axios from "axios";
 
 // Async Thunk to fetch products by collection and optional filters
 export const fetchProductsByFilters = createAsyncThunk(
- "products/fetchByFilters",
-  async({ collection, size, color, gender, minPrice, maxPrice, sortBy, search, category, material, brand, limit }) => {
-     const query = new URLSearchParams()
-     if (collection) query.append("collection", collection)
-     if (size) query.append("size", size)
-     if (color) query.append("color", color)
-     if (gender) query.append("gender", gender)
-     if (minPrice) query.append("minPrice", minPrice)
-     if (maxPrice) query.append("maxPrice", maxPrice)
-     if (sortBy) query.append("sortBy", sortBy)
-     if (search) query.append("search", search)
-     if (category) query.append("category", category)
-     if (material) query.append("material", material)
-     if (brand) query.append("brand", brand)
-     if (limit) query.append("limit", limit)
+    "products/fetchByFilters",
+    async ({ collection, size, color, gender, minPrice, maxPrice, sortBy, search, category, material, brand, limit }) => {
+        const query = new URLSearchParams()
+        if (collection) query.append("collection", collection)
+        if (size) query.append("size", size)
+        if (color) query.append("color", color)
+        if (gender) query.append("gender", gender)
+        if (minPrice) query.append("minPrice", minPrice)
+        if (maxPrice) query.append("maxPrice", maxPrice)
+        if (sortBy) query.append("sortBy", sortBy)
+        if (search) query.append("search", search)
+        if (category) query.append("category", category)
+        if (material) query.append("material", material)
+        if (brand) query.append("brand", brand)
+        if (limit) query.append("limit", limit)
 
-     const response = await axios.get(
-        `${import.meta.env.VITE_BACKEND_URL}/api/products?${query.toString()}`
-    )
+        const response = await axios.get(
+            `${import.meta.env.VITE_BACKEND_URL}/api/products?${query.toString()}`
+        )
 
-    return response.data
-  }
+        return response.data
+    }
 )
 
 
 // Async thunk to fetch a single product by ID
 export const fetchProductDetails = createAsyncThunk(
     "products/fetchProductDetails",
-    async({ id }) => {
-       const response = await axios.get(
-        `${import.meta.env.VITE_BACKEND_URL}/api/products/${id}`
-       ) 
+    async ({ id }) => {
+        const response = await axios.get(
+            `${import.meta.env.VITE_BACKEND_URL}/api/products/${id}`
+        )
 
-       return response.data
+        return response.data
     }
 
 )
 
 // Async thunk to fetch similar products
 export const updateProduct = createAsyncThunk
-(
-  "products/updateProduct",
-  async ({ id, productData }) => {
-    const response = await axios.put(
-        `${import.meta.env.VITE_BACKEND_URL}/api/products/${id}`,
-        productData,
-        {
-            headers: {
-              authorization: `Bearer ${localStorage.getItem("userToken")}`
-            }
+    (
+        "products/updateProduct",
+        async ({ id, productData }) => {
+            const response = await axios.put(
+                `${import.meta.env.VITE_BACKEND_URL}/api/products/${id}`,
+                productData,
+                {
+                    headers: {
+                        authorization: `Bearer ${localStorage.getItem("userToken")}`
+                    }
+                }
+            )
+
+            return response.data
         }
     )
-
-    return response.data
-  }
-)
 
 // Async thunk to fetch similar products
 export const fetchSimilarProducts = createAsyncThunk(
     "products/fetchSimilarProducts",
     async ({ id }) => {
-      const response = await axios.get(
-        `${import.meta.env.VITE_BACKEND_URL}/api/products/similar/${id}`
-      ) 
-      return response.data
+        const response = await axios.get(
+            `${import.meta.env.VITE_BACKEND_URL}/api/products/similar/${id}`
+        )
+        return response.data
     }
 )
 
 const productsSlice = createSlice({
+    name: "products",
     initialState: {
+        products: [],
+        selectedProduct: null, // Store the details of the single Product
+        similarProducts: [],
+        loading: false,
+        error: null,
+        filters: {
+            category: "",
+            size: "",
+            color: "",
+            gender: "",
+            brand: "",
+            minPrice: "",
+            maxPrice: "",
+            sortBy: "",
+            search: "",
+            material: "",
+            collection: "",
+        }
+    },
+    reducers: {
+        setFilters: (state, action) => {
+            state.filters = { ...state.filters, ...action.payload }
+        },
+        clearFilters: (state) => {
+            state.filters = {
+                category: "",
+                size: "",
+                color: "",
+                gender: "",
+                brand: "",
+                minPrice: "",
+                maxPrice: "",
+                sortBy: "",
+                search: "",
+                material: "",
+                collection: "",
+            }
+        }
+    },
+    extraReducers: (builder) => {
+        builder
+        // handle fetching products with filter
+        .addCase(fetchProductsByFilters.pending, (state) => {
+            state.loading = true;
+            state.error = null;
+        })
+        .addCase(fetchProductsByFilters.fulfilled, (state, action) => {
+            state.loading = false;
+            state.products = Array.isArray(action.payload) ? action.payload : []
+            state.error = null;
+        })
+        .addCase(fetchProductsByFilters.rejected, (state) => {
+            state.loading = false;
+            state.error = action.error.message;
+        })
         
     }
 })
