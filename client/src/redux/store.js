901340c3bdf39cbde18a46ce255bac7eefb9de3f
Ahnibaba <aniebietedet2025@gmail.com>
2025-07-26 07:47:30 +0100

    adminOrderSlice

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -6,6 +6,7 @@ import checkoutReducer from "./slices/checkoutSlice"
 import orderReducer from "./slices/orderSlice"
 import adminReducer from "./slices/adminSlice"
 import adminProductReducer from "./slices/adminProductSlice"
+import adminOrderReducer from "./slices/adminOrderSlice"
 
 const store = configureStore({
     reducer: {
@@ -15,7 +16,8 @@ const store = configureStore({
         checkout: checkoutReducer,
         order: orderReducer,
         admin: adminReducer,
-        adminProducts: adminProductReducer
+        adminProducts: adminProductReducer,
+        adminOrder: adminOrderReducer,
     },
 })
 
